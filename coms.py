aa25d9dd736fe68812704e920bffb11dde16d9e4
haaaaruuuu <77024043+haaaaruuuu@users.noreply.github.com>
2021-03-18 18:44:47 -0700

    Update coms.py

--- coms.py
+++ coms.py
@@ -1,13 +1,9 @@
-# discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import discord
 
-import schedule
-
 from bot import glonkB
-from boards import get_info
-from boards import set_info
 from boards import build_board
+from boards import error_embed
 
 import os
 from dotenv import load_dotenv
@@ -19,14 +15,35 @@ yt_channel = os.getenv('S_YOUTUBE')
 class glonkCog(commands.Cog):
     def __init__(self, bot: glonkB):
         self.bot = bot
+        self.update_board.start()
 
     @commands.command()
     async def glonk(self, ctx):
         ytboard = await build_board()
         message = await ctx.send(embed = ytboard)
-        with open('board.txt', 'w') as x:
+        with open('boardmsg.txt', 'w') as x:
             x.write(str(message.id))
-            x.close()
+        with open('boardchan.txt', 'w') as y:
+            y.write(str(message.channel.id))
+    
+    @tasks.loop(minutes=15.0)
+    async def update_board(self):
+        with open('boardmsg.txt') as x:
+            msg_id = "".join(x.readlines())
+        with open('boardchan.txt') as y:
+            msg_channel = "".join(y.readlines())
+        msg_id = int(msg_id)
+        msg_channel = int(msg_channel)
+        
+        channel = self.bot.get_channel(msg_channel)
+        msg = await channel.fetch_message(msg_id)
+
+        embed = await build_board()
+        await msg.edit(embed=embed)
+    
+    @update_board.before_loop
+    async def before_update_board(self):
+        await self.bot.wait_until_ready()
 
 def setup(bot):
     bot.add_cog(glonkCog(bot))
